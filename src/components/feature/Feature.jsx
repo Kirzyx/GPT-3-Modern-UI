b31b7cfc53b4ae9c6970bcb3dfe23aa484712f49
Ryan Luo <hluo1@seattleu.edu>
2023-08-17 17:29:17 -0700

    Add Home Page and "What is GPT-3" Page
    Added the initial structure and content for the home page, providing a welcoming introduction to the website.
    
    Also, introduced a dedicated page titled "What is GPT-3" with information about the technology, its applications, and its significance.
    
    The navigation menu has been updated to include links to both pages for easy access.

---
+++ src/components/feature/Feature.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './feature.css'
+
+const Feature = ({ title, text }) => {
+  return (
+    <div className="gpt3__features-container_feature">
+      <div className="gpt3__features-container__feature-title">
+        <h1>{title}</h1>
+      </div>
+      <div className="gpt3__features-container_feature-text">
+        {text}
+      </div>
+    </div>
+  )
+}
+
+export default Feature;
---
+++ src/containers/possiblity/Possiblity.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './possiblity.css'
+
+
+const Possiblity = () => {
+  return (
+    <div>
+        Possiblity
+    </div>
+  )
+}
+
+export default Possiblity
