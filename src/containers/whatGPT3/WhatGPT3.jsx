1da17a343c44d1e79a5a79130e49d643fe566a8a
Ryan Luo <hluo1@seattleu.edu>
2023-08-18 17:02:18 -0700

    articles

--- src/containers/whatGPT3/WhatGPT3.jsx
+++ src/containers/whatGPT3/WhatGPT3.jsx
@@ -5,16 +5,16 @@ import './whatGPT3.css';
 const WhatGPT3 = () => (
   <div className="gpt3__whatgpt3 section__margin" id="wgpt3">
     <div className="gpt3__whatgpt3-feature">
-      <Feature title="What is GPT-3" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought. His defective nor convinced residence own. Connection has put impossible own apartments boisterous. At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by." />
+      <Feature title="What is GPT-3" text="Within the digital expanse, GPT-3 emerges as a radiant marvel. A culmination of code and data, each interaction unlocks its potential further. Minds intrigued, possibilities unfurl, as we delve into the capabilities of this artificial curiosity, propelling us towards new frontiers of understanding." />
     </div>
     <div className="gpt3__whatgpt3-heading">
       <h1 className="gradient__text">The possibilities are beyond your imagination</h1>
       <p>Explore the Library</p>
     </div>
     <div className="gpt3__whatgpt3-container">
-      <Feature title="Chatbots" text="We so opinion friends me message as delight. Whole front do of plate heard oh ought." />
-      <Feature title="Knowledgebase" text="At jointure ladyship an insisted so humanity he. Friendly bachelor entrance to on by. As put impossible own apartments" />
-      <Feature title="Education" text="In the world of education, wisdom awakens through shared exploration. Every question is a seed, every discovery a beacon, illuminating the path to endless enlightenment." />
+      <Feature title="Chatbots" text="Unveiling new conversational frontiers. Chatbots excel in delivering delightful opinions and messages, resembling a finely-tuned AI plate serving responses. With their dynamic virtual presence, they redefine interaction, promising endless possibilities." />
+      <Feature title="Knowledgebase" text= "Knowledgebase flourishes as a guiding constellation. Each entry, a stepping stone; every query, a pathway to understanding. Minds kindled, curiosity sets sail, as the exploration of insights propels our collective journey toward limitless horizons." />
+      <Feature title="Education" text="Within the realm of education, knowledge blooms as a guiding light. Each lesson, a stepping stone; every question, a pathway to insight. Minds ignited, curiosity takes flight, as the quest for understanding fuels our shared journey toward boundless heights." />
     </div>
   </div>
 );
